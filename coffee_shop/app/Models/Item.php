d9ca7521045273d919d6e2c5bbdd680304186ebc
rabiul <rabisefat@gmail.com>
2023-09-22 21:07:04 +0600

    laravel project model create

---
+++ coffee_shop/app/Models/Item.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    protected $fillable =[
+        'coffee_name',
+        'coffee_desc',
+        'coffee_price'
+    ];
+}
